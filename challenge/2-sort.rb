b15b9c19a9bdca2a7673c10272fffe22d3dcce3f
movlan17 <movlanomerov00@gmail.com>
2026-03-20 10:03:47 +0400

    df

--- challenge/2-sort.rb
+++ challenge/2-sort.rb
@@ -1,30 +1,12 @@
 #!/usr/bin/env ruby
 
-# Remove the first argument (script name)
-args = ARGV
+# Filter only integers from arguments
+numbers = ARGV.select { |arg| arg.match?(/^-?\d+$/) }.map(&:to_i)
 
-# Separate integers and strings
-integers = []
-strings = []
+# Sort numbers in ascending order
+numbers.sort!
 
-args.each do |arg|
-  if arg.match?(/^-?\d+$/)
-    integers << arg.to_i
-  else
-    strings << arg
-  end
-end
-
-# Sort integers ascending
-integers.sort!
-
-# Sort strings alphabetically
-strings.sort!
-
-# Combine results: integers first, then strings
-result = integers + strings
-
-# Print each element on new line
-result.each do |item|
-  puts item
+# Print each number on a new line
+numbers.each do |num|
+  puts num
 end
